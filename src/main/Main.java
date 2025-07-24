d5f274bf21b224699a328c7b6eeae399c2edb85b
Komal Warake <warkekomal04@gmail.com>
2025-07-24 21:42:19 +0530

    Database connected

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,15 @@
-public class Main {
-    public static void main(String args[]){
+package main;
 
+import util.DBConnection;
+import java.sql.Connection;
+
+public class Main {
+    public static void main(String[] args) {
+        Connection conn = DBConnection.getConnection();
+        if (conn != null) {
+            System.out.println("Database Connected Successfully!");
+        } else {
+            System.out.println("Connection Failed");
+        }
     }
 }
--- src/util/DBConnection.java
+++ src/util/DBConnection.java
@@ -1,5 +1,24 @@
 package util;
 
-public class DBConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
-}
+    public class DBConnection {
+        private static final String URL = "jdbc:mysql://localhost:3306/Banking_Management_System";
+        private static final String USER = "root";
+        private static final String PASSWORD = "";
+
+        public static Connection getConnection() {
+            Connection conn = null;
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return conn;
+        }
+    }
+
+
+
