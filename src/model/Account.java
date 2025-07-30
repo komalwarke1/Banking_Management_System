23723e939869def5d9d3b22d236eaa5e96cc5372
Komal Warake <warkekomal04@gmail.com>
2025-07-30 10:29:15 +0530

    getter setter added

--- src/model/Account.java
+++ src/model/Account.java
@@ -1,4 +1,52 @@
 package model;
 
 public class Account {
+    private int id;
+    private int userid;
+    private String account_number;
+    private double balance;
+
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public int getUserid(){
+        return userid;
+    }
+    public void setUserid(int userid){
+        this.userid=userid;
+    }
+    public String getAccount_number(){
+        return account_number;
+    }
+    public void setAccount_number(String account_number){
+        this.account_number=account_number;
+    }
+    public double getBalance(){
+        return balance;
+    }
+    public void setBalance(double balance){
+        this.balance=balance;
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
