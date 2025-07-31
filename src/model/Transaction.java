c258f65b7fe3a37b420587294d36db845fc406fb
Komal Warake <warkekomal04@gmail.com>
2025-07-31 10:13:59 +0530

    getters setters added for transaction

--- src/model/Transaction.java
+++ src/model/Transaction.java
@@ -1,4 +1,42 @@
 package model;
 
+import java.security.Timestamp;
+
 public class Transaction {
+    private int id;
+    private int accountId;
+    private String type;
+    private double amount;
+    private Timestamp date;
+
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public int getAccountId(){
+        return accountId;
+    }
+    public void setAccountId(int accountId){
+        this.accountId=accountId;
+    }
+    public String getType(){
+        return type;
+    }
+    public void setType(String type){
+        this.type=type;
+    }
+    public double getAmount(){
+        return amount;
+    }
+    public void setAmount(double amount){
+        this.amount=amount;
+    }
+    public Timestamp getDate(){
+        return date;
+    }
+    public void setDate(Timestamp date){
+        this.date=date;
+    }
 }
